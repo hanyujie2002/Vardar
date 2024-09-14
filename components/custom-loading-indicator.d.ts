495e074f270024ed1823cc732fcc497ffac95d26
hanyujie2002 <84226578+hanyujie2002@users.noreply.github.com>
2024-09-14 19:23:53 +0800

    feat(loading indicator): Implement loading indicator

---
+++ components/custom-loading-indicator.d.ts
@@ -0,0 +1,43 @@
+declare const _default: import("vue").DefineComponent<import("vue").ExtractPropTypes<{
+    throttle: {
+        type: NumberConstructor;
+        default: number;
+    };
+    duration: {
+        type: NumberConstructor;
+        default: number;
+    };
+    height: {
+        type: NumberConstructor;
+        default: number;
+    };
+    color: {
+        type: (BooleanConstructor | StringConstructor)[];
+        default: string;
+    };
+    errorColor: {
+        type: StringConstructor;
+        default: string;
+    };
+    estimatedProgress: {
+        type: () => (duration: number, elapsed: number) => number;
+        required: false;
+    };
+}>, () => import("vue").VNode<import("vue").RendererNode, import("vue").RendererElement, {
+    [key: string]: any;
+}>, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<{
+    color: string | boolean;
+    height: number;
+    duration: number;
+    throttle: number;
+    errorColor: string;
+} & {
+    estimatedProgress?: ((duration: number, elapsed: number) => number) | undefined;
+} & {}>, {
+    color: string | boolean;
+    height: number;
+    duration: number;
+    throttle: number;
+    errorColor: string;
+}, {}, {}, {}, string, import("vue").ComponentProvideOptions, true, {}>;
+export default _default;
