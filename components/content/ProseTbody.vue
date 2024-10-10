b789edcd578be36497ee244cdadc9333426b1b05
hanyujie2002 <84226578+hanyujie2002@users.noreply.github.com>
2024-10-10 13:21:07 +0800

    feat: Enhanced style of table

--- components/content/ProseTbody.vue
+++ components/content/ProseTbody.vue
@@ -1,5 +1,7 @@
 <template>
-  <tbody class="divide-y divide-themeColor-400 border-b">
+  <tbody
+    class="divide-y divide-themeColor-400/50 border-b border-themeColor-400/50 dark:divide-themeColor-100/50 dark:border-themeColor-100/50"
+  >
     <slot />
   </tbody>
 </template>
--- components/content/ProseThead.vue
+++ components/content/ProseThead.vue
@@ -1,6 +1,6 @@
 <template>
   <thead
-    class="sticky top-0 z-10 border-b bg-themeColor-100 text-themeColor-600"
+    class="sticky top-0 z-10 bg-themeColor-100 text-themeColor-600 dark:bg-themeColor-800/85 dark:text-themeColor-100"
   >
     <slot />
   </thead>
