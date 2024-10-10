00be40ddabdc3200224efc7b34ee24a511c92715
hanyujie2002 <84226578+hanyujie2002@users.noreply.github.com>
2024-10-10 21:32:46 +0800

    fix: Hydration completed but contains mismatches error

--- components/ColorModeSwitch.vue
+++ components/ColorModeSwitch.vue
@@ -1,23 +1,25 @@
 <template>
-  <button
-    title="Switch Color Mode"
-    aria-label="Switch Color Mode"
-    @click="switchColorMode"
-  >
-    <div v-if="colorMode.value === 'dark'">
-      <MaterialSymbolsDarkModeOutlineRounded
-        class="size-10 transition-colors hover:text-yellow-500 active:text-yellow-400 sm:size-12 dark:hover:text-yellow-100 dark:active:text-yellow-200"
-      />
-    </div>
-    <div v-else-if="colorMode.value === 'light'">
-      <MaterialSymbolsSunnyOutlineRounded
-        class="size-10 transition-colors hover:text-yellow-500 active:text-yellow-400 sm:size-12 dark:hover:text-yellow-100 dark:active:text-yellow-200"
-      />
-    </div>
-    <div v-else>
-      <div class="size-10 sm:size-12" />
-    </div>
-  </button>
+  <ClientOnly>
+    <button
+      title="Switch Color Mode"
+      aria-label="Switch Color Mode"
+      @click="switchColorMode"
+    >
+      <div v-if="colorMode.value === 'dark'">
+        <MaterialSymbolsDarkModeOutlineRounded
+          class="size-10 transition-colors hover:text-yellow-500 active:text-yellow-400 sm:size-12 dark:hover:text-yellow-100 dark:active:text-yellow-200"
+        />
+      </div>
+      <div v-else-if="colorMode.value === 'light'">
+        <MaterialSymbolsSunnyOutlineRounded
+          class="size-10 transition-colors hover:text-yellow-500 active:text-yellow-400 sm:size-12 dark:hover:text-yellow-100 dark:active:text-yellow-200"
+        />
+      </div>
+      <div v-else>
+        <div class="size-10 sm:size-12" />
+      </div>
+    </button>
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
