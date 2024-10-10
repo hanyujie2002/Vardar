edc187b74dc4ad711535d70ecee7f6d9a4b44706
hanyujie2002 <84226578+hanyujie2002@users.noreply.github.com>
2024-10-10 20:55:34 +0800

    feat: Updated style of github flavored alert block (part 3)

--- app.vue
+++ app.vue
@@ -22,35 +22,35 @@
 }
 
 .admonition-note {
-  @apply rounded-r border border-l-4 border-blue-500 bg-white p-2 sm:p-4 dark:border-blue-600 dark:bg-blue-800;
+  @apply rounded-r border border-l-4 border-blue-200 bg-white p-2 shadow-lg shadow-blue-100 sm:p-4 dark:border-blue-600 dark:bg-blue-800 dark:shadow-blue-900;
 }
 .admonition-note-title {
   @apply font-bold;
 }
 
 .admonition-warning {
-  @apply rounded-r border border-l-4 border-amber-500 bg-white p-2 sm:p-4 dark:border-amber-600 dark:bg-amber-800;
+  @apply rounded-r border border-l-4 border-amber-200 bg-white p-2 shadow-lg shadow-amber-100 sm:p-4 dark:border-amber-600 dark:bg-amber-800 dark:shadow-amber-900;
 }
 .admonition-warning-title {
   @apply font-bold;
 }
 
 .admonition-important {
-  @apply rounded-r border border-l-4 border-red-500 bg-white p-2 sm:p-4 dark:border-red-600 dark:bg-red-800;
+  @apply rounded-r border border-l-4 border-red-200 bg-white p-2 shadow-lg shadow-red-100 sm:p-4 dark:border-red-600 dark:bg-red-800 dark:shadow-red-900;
 }
 .admonition-important-title {
   @apply font-bold;
 }
 
 .admonition-tip {
-  @apply rounded-r border border-l-4 border-green-500 bg-white p-2 sm:p-4 dark:border-green-600 dark:bg-green-800;
+  @apply rounded-r border border-l-4 border-green-200 bg-white p-2 shadow-lg shadow-green-100 sm:p-4 dark:border-green-600 dark:bg-green-800 dark:shadow-green-900;
 }
 .admonition-tip-title {
   @apply font-bold;
 }
 
 .admonition-caution {
-  @apply rounded-r border border-l-4 border-orange-500 bg-white p-2 sm:p-4 dark:border-orange-600 dark:bg-orange-800;
+  @apply rounded-r border border-l-4 border-orange-200 bg-white p-2 shadow-lg shadow-orange-100 sm:p-4 dark:border-orange-600 dark:bg-orange-800 dark:shadow-orange-900;
 }
 .admonition-caution-title {
   @apply font-bold;
