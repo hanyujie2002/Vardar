1eba1ecbcac364ba34a9101df508a91b81f7196d
hanyujie2002 <84226578+hanyujie2002@users.noreply.github.com>
2024-10-11 11:48:21 +0800

    feat: Added icons for github flavored alart blocks (part 2)

--- app.vue
+++ app.vue
@@ -81,10 +81,10 @@
   @apply font-bold;
 }
 .admonition-caution-title::before {
-  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24'%3E%3Cg fill='none' stroke='%23fdba74'%3E%3Cpath stroke-linecap='round' stroke-linejoin='round' stroke-width='3' d='M13.253 5.98L12 13.5l-1.253-7.52a1.27 1.27 0 1 1 2.506 0'/%3E%3Ccircle cx='12' cy='19' r='1' stroke-width='2'/%3E%3C/g%3E%3C/svg%3E");
+  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='0.9em' height='0.9em' viewBox='0 0 24 24'%3E%3Cg fill='none' stroke='%23fdba74'%3E%3Cpath stroke-linecap='round' stroke-linejoin='round' stroke-width='3' d='M13.253 5.98L12 13.5l-1.253-7.52a1.27 1.27 0 1 1 2.506 0'/%3E%3Ccircle cx='12' cy='19' r='1' stroke-width='2'/%3E%3C/g%3E%3C/svg%3E");
 }
 .dark .admonition-caution-title::before {
-  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24'%3E%3Cg fill='none' stroke='%23fed7aa'%3E%3Cpath stroke-linecap='round' stroke-linejoin='round' stroke-width='3' d='M13.253 5.98L12 13.5l-1.253-7.52a1.27 1.27 0 1 1 2.506 0'/%3E%3Ccircle cx='12' cy='19' r='1' stroke-width='2'/%3E%3C/g%3E%3C/svg%3E");
+  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='0.9em' height='0.9em' viewBox='0 0 24 24'%3E%3Cg fill='none' stroke='%23fed7aa'%3E%3Cpath stroke-linecap='round' stroke-linejoin='round' stroke-width='3' d='M13.253 5.98L12 13.5l-1.253-7.52a1.27 1.27 0 1 1 2.506 0'/%3E%3Ccircle cx='12' cy='19' r='1' stroke-width='2'/%3E%3C/g%3E%3C/svg%3E");
 }
 
 [id^="user-content-fn-"] {
