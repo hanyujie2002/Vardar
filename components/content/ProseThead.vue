466238140a6145a3d1fa2612660834f2f86247d4
hanyujie2002 <84226578+hanyujie2002@users.noreply.github.com>
2024-10-11 20:08:35 +0800

    fix: Fixed text shown behind table header

--- components/content/ProseThead.vue
+++ components/content/ProseThead.vue
@@ -1,6 +1,6 @@
 <template>
   <thead
-    class="sticky top-0 z-10 bg-themeColor-100 text-themeColor-600 transition-all duration-1000 dark:bg-themeColor-800/85 dark:text-themeColor-100"
+    class="sticky top-0 z-10 bg-themeColor-100 text-themeColor-600 transition-all duration-1000 dark:bg-themeColor-800 dark:text-themeColor-100"
   >
     <slot />
   </thead>
