781e996c5ec0747d621986487c50b7c11a203d23
hanyujie2002 <84226578+hanyujie2002@users.noreply.github.com>
2024-10-12 18:17:46 +0800

    feat: Added support for more emojis via twemoji mozilla font

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,11 @@ module.exports = {
     './error.vue',
   ],
   theme: {
+    fontFamily: {
+      'sans': ["ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Twemoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      'serif': ["ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
+      'mono': ["ui-monospace", "SFMono-Regular", "Menlo", "Monaco", "Consolas", "Liberation Mono", "Courier New", "monospace"]
+    },
     extend: {
       animation: {
         spinSlow: 'spin 10s linear infinite',
