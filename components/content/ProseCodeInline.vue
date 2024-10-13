4e0680ba2c11d3f2493919eda55b24fbd82b76b7
hanyujie2002 <84226578+hanyujie2002@users.noreply.github.com>
2024-10-13 17:42:28 +0800

    feat: Updated style of inline code blocks

--- components/content/ProseCodeInline.vue
+++ components/content/ProseCodeInline.vue
@@ -1,6 +1,6 @@
 <template>
   <code
-    class="rounded bg-themeColor-200 px-1 font-mono transition-colors dark:bg-themeColor-800"
+    class="rounded bg-white border border-themeColor-200 px-1 font-mono transition-all duration-1000 dark:bg-themeColor-950 dark:border-transparent"
   >
     <slot />
   </code>
